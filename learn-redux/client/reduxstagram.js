4773697ec756379b56b8e7c7cfbe63645bd7902b
GiadaSimonetti <giada.simonetti@made.com>
2018-08-10 11:58:16 +0100

    Render the Main component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,5 +1,12 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// import css from './styles/style.styl';
+//
+// render(<p>hi</p>, document.getElementById('root'));
+
 import React from 'react';
 import { render } from 'react-dom';
+import Main from './components/Main';
 import css from './styles/style.styl';
 
-render(<p>hi</p>, document.getElementById('root'));
+render(<Main/>, document.getElementById('root'));
